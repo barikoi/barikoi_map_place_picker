c868e6d1a2bf37f2693628addf300a8c34f39e37
Sakib <shadmanshakib1@gmail.com>
2022-11-16 12:21:05 +0600

    optimized imports

--- lib/barikoi_maps_place_picker.dart
+++ lib/barikoi_maps_place_picker.dart
@@ -4,3 +4,4 @@ export 'src/models/pick_result.dart';
 export 'src/components/floating_card.dart';
 export 'src/components/rounded_frame.dart';
 export 'src/place_picker.dart';
+export 'package:maplibre_gl_platform_interface/maplibre_gl_platform_interface.dart';
